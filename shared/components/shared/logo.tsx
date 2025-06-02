ff37ec37e81a5d9de119c5da43f41c309a090be5
Illia Bilous <bilous.job@yahoo.com>
2025-06-02 23:12:44 +0300

    feat: logo component

---
+++ shared/components/shared/logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+export const Logo = () => {
+  return (
+    <div className="flex gap-2 items-center">
+        <Image 
+            src="/logo.png"
+            alt="Logo"
+            width={40}
+            height={40}
+        />
+        <h1 className="text-[2rem] tracking-[-0.03em] leading-[calc(1.24*2rem)] font-semibold text-gray-900">E-Tutor</h1>
+    </div>
+  )
+}
