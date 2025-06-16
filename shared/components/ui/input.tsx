97905819e80be6043abf64bd5f968793c9e47e38
Illia Bilous <bilous.job@yahoo.com>
2025-06-16 20:10:50 +0300

    feat: label, input

---
+++ shared/components/ui/input.tsx
@@ -0,0 +1,46 @@
+import * as React from "react"
+
+import { cn } from "@/shared/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const inputVariants = cva(
+  `flex h-12 w-full min-w-0 border px-4.5 py-3 outline-none
+   placeholder:typo-body-lg-400 typo-body-lg-400 hover:border-primary-200 
+   focus:border-primary-500 focus:placeholder:text-gray-300 focus:text-gray-900
+  `,
+  {
+    variants: {
+      variant: {
+        default: "placeholder:text-gray-500 border-gray-100 bg-white text-gray-700",
+      },
+      state: {
+        default: "",
+        invalid: "border-error-300 focus:border-red-500",
+        valid: "border-green-500 focus:border-green-500",
+      },
+    },
+    defaultVariants: {
+      state: "default",
+      variant: "default",
+    },
+  }
+)
+
+function Input({ 
+  variant, 
+  state,
+  type, 
+  ...props 
+}: React.ComponentProps<"input"> 
+& VariantProps<typeof inputVariants>) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(inputVariants({ variant, state }))}
+      {...props}
+    />
+  )
+}
+
+export { Input }
