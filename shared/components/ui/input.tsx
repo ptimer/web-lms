72a39f88e4daaf0b9948c10410af88e806d43505
Illia Bilous <bilous.job@yahoo.com>
2025-06-17 20:59:19 +0300

    feat: status for input

--- shared/components/ui/input.tsx
+++ shared/components/ui/input.tsx
@@ -1,45 +1,78 @@
+'use client'
+
 import * as React from "react"
 
 import { cn } from "@/shared/lib/utils"
 import { cva, type VariantProps } from "class-variance-authority"
 
-const inputVariants = cva(
-  `flex h-48 w-full min-w-0 border px-18 py-12 outline-none
-   placeholder:typo-body-lg-400 typo-body-lg-400 hover:border-primary-200 
-   focus:border-primary-500 focus:placeholder:text-gray-300 focus:text-gray-900
-  `,
+import { WarningIcon, CheckIcon } from "@phosphor-icons/react";
+
+const containerVariants = cva(
+  `flex h-48 border px-18 py-12 border-gray-100 bg-white items-center`,
   {
     variants: {
-      variant: {
-        default: "placeholder:text-gray-500 border-gray-100 bg-white text-gray-700",
-      },
-      state: {
-        default: "",
-        invalid: "border-error-300 focus:border-red-500",
-        valid: "border-green-500 focus:border-green-500",
+      status: {
+        default: "hover:border-primary-200 hover:border-1 focus:border-primary-500",
+        error: "border-error-300 bg-[rgba(249,234,234,0.5)] ",
+        success: "border-success-300 bg-[rgba(223,242,225,0.5)]",
       },
     },
     defaultVariants: {
-      state: "default",
-      variant: "default",
+      status: "default",
     },
   }
 )
 
+const inputVariants = cva(
+  `outline-none w-full
+   placeholder:typo-body-lg-400 typo-body-lg-400
+   focus:placeholder:text-gray-300 focus:text-gray-900
+   placeholder:text-gray-500 text-gray-700
+  `
+);
+
+interface Props extends React.ComponentProps<"input">, VariantProps<typeof containerVariants> {
+  startContent?: React.ReactNode;
+  endContent?: React.ReactNode;
+}
+
+const composeStatusIcon = (status: Props['status']) => {
+  const className = 'h-19 w-19 px-4 py-5 flex items-center justify-center rounded-full';
+
+  switch(status) {
+    case "error":
+      return (
+        <div className={cn(className, "bg-error-500")}>
+          <WarningIcon color="var(--color-white)" weight="bold" />
+        </div>
+      )
+    case "success":
+      return (
+        <div className={cn(className, "bg-success-500")}>
+          <CheckIcon color="var(--color-white)" weight="bold" />
+        </div>
+      )
+    default:
+      return null;
+  }
+}
+
 function Input({ 
-  variant, 
-  state,
-  type, 
+  status,
+  startContent,
+  endContent = composeStatusIcon(status),
   ...props 
-}: React.ComponentProps<"input"> 
-& VariantProps<typeof inputVariants>) {
+}: Props) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(inputVariants({ variant, state }))}
-      {...props}
-    />
+    <div className={cn(containerVariants({ status }))}>
+      {startContent && <>{startContent}</>}
+      <input
+        data-slot="input"
+        className={cn(inputVariants())}
+        {...props}
+      />
+      {endContent && <>{endContent}</>}
+    </div>
   )
 }
 
